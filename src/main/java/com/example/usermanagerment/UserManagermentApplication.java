37dff73685a4ab39efeaf607ce51633cf4c7cbfa
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-07-06 15:13:58 +0700

    vinh

---
+++ src/main/java/com/example/usermanagerment/UserManagermentApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagerment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagermentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagermentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usermanagerment/UserManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermanagerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagermentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
